f7abdd3c80d209b20d75649c41c4fc8c2dbb8eb5
John Doe <aslantasezginn@gmail.com>
2023-01-02 22:46:17 +0300

    multiple reducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 
 function App() {
+
+  const userState = useSelector(state => state.user)
+  const countState = useSelector(state => state.count)
+  const dispatch = useDispatch()
+
+  const [userName, _setUserName] = useState("")
+
   return (
     <div className="App">
       <header className="App-header">
-       
+        <input type="text" onChange={(e) => {
+           _setUserName(e.target.value)
+        }}></input>
+        <p>NAME : {userState.userName} </p>
+        <button onClick={() => {
+          dispatch({
+            type:"SET_USERNAME",
+            payload:userName
+          })
+        }}>Change Name</button>
+
+        
+
+      <p>SAYI :{countState.count}</p>
+      <button onClick={() => {
+        dispatch({
+          type:"INCREMENT",
+          payload:5
+
+        })
+      }} >Arttır</button>
+
+<button onClick={() => {
+        dispatch({
+          type:"DECREMENT",
+          payload:5
+
+        })
+      }} >Azalt</button>
       </header>
     </div>
   );
